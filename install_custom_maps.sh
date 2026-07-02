7256ec45541a40b1ea64e2626e296fe3fe9f7913
robex <23484075+robex@users.noreply.github.com>
2026-07-02 17:30:35 +0200

    update custom maps url

--- install_custom_maps.sh
+++ install_custom_maps.sh
@@ -4,5 +4,5 @@
 # Download custom maps (comment if wanted, requires around 40GB disk space)
 cd /home/steam/l4d2/addons/
 
-wget https://sirplease.vercel.app/downloads/maps/allmaps.zip
-unzip allmaps.zip
+wget https://sirplease.vercel.app/downloads/maps/L4D2Maps.zip
+unzip L4D2Maps.zip
